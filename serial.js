54f50afc267d5e0b390057463b067957efed63ce
Hernán Eche <hernan_eche@yahoo.com.ar>
2016-01-12 10:05:58 -0300

    Update serial.js

--- serial.js
+++ serial.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var serialport = require("serialport");
-var readline = serialport.parsers.readline;
-var SerialPort = serialport.SerialPort; // localize object constructor
+var serialport = require("serialport");         //load serial port library object
+var readline = serialport.parsers.readline;     //parser-separator for data input
+var SerialPort = serialport.SerialPort;         //localize object constructor
 
 
 //FORMA INCOMPRENSIBLE DE CREAR UN SERVER USANDO CANTIDAD MAXIMA DE LIBRERIAS
@@ -16,40 +16,35 @@ server.listen(80);
 
 //ATIENDE METODO GET
 appExpress.get('/', function (req, res) {
-
     res.sendFile(__dirname + '/public/' + 'graph.html');
 });
 
-
-if (io) {
+if (io) {   //server created 
     console.log('listening');
+
+    var sp = null;
+    io.sockets.on('connection', function (socket) {
+
+        if (sp === null) {
+            sp = new SerialPort("COM77", {
+                baud: 57600,
+                parser: readline('\r')
+            });
+
+            sp.on("open", function () {
+                console.log('open serial port');
+                sp.on('data', function (data) {
+                    console.log('data received: ' + data.toString('hex'));
+                    socket.emit('newServerData', data);
+                });
+
+                sp.on('error', function (data) {
+                    console.log('error');
+                });
+            });
+        }
+        socket.on('clientData', function (clientData) {
+            console.log("client" + clientData);
+        });
+    });
 }
-var sp=null;
-io.sockets.on('connection', function (socket) {
-     
-    if(sp===null){
-        sp = new SerialPort("COM77", {
-        baud: 57600,
-        parser: readline('\r')
-    });
-
-    sp.on("open", function () {
-        console.log('open serial port');
-        sp.on('data', function (data) {
-            console.log('data received: ' + data.toString('hex'));
-            socket.emit('newServerData', data);
-        });
-
-        sp.on('error', function (data) {
-            console.log('error');
-        });
-    });
-    }
-    socket.on('clientData', function (clientData) {
-        console.log("client"+clientData);
-    });
-
- 
-
-});
-
